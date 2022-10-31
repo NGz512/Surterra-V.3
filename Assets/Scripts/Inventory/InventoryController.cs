dc81c33d2385b38852d539d33e0ff43586b54621
Laylays <amklin_k@silpakorn.edu>
2022-10-31 18:40:27 +0700

    Toolbar

--- Assets/Scripts/Inventory/InventoryController.cs
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class InventoryController : MonoBehaviour
 {
     [SerializeField] GameObject panel;
-
+    [SerializeField] GameObject toolbarPanel;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.I))
         {
             panel.SetActive(!panel.activeInHierarchy);
+            toolbarPanel.SetActive(!toolbarPanel.activeInHierarchy);
         }
     }
 }
